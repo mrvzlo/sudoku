b3c71ff59ef22f37b885cb45d8188e97fc04aa82
Vladimir Andrejev <andrejevve@gmail.com>
2023-10-05 23:22:53 +0300

    Updates

---
+++ precache-manifest.dafb814152b7cf551ca7f5947c8b391d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057a85bdcb8317dc0618",
+    "url": "/css/app.6062a3c6.css"
+  },
+  {
+    "revision": "156293b684fc9b861ddc0ef94a38fdc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "057a85bdcb8317dc0618",
+    "url": "/js/app.f37a4efe.js"
+  },
+  {
+    "revision": "a8c42774981f5e977db2",
+    "url": "/js/chunk-vendors.09355b1b.js"
+  },
+  {
+    "revision": "d558a00e72625918a60d403a1ba4509e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
